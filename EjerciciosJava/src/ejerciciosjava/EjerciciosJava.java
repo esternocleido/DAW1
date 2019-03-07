0c17ffc8ad6a059b97458968b2b7324dea96156f
palsandi <palsandi@192.168.56.1>
2019-03-07 10:11:16 +0100

    Pruebita

---
+++ EjerciciosJava/src/ejerciciosjava/EjerciciosJava.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejerciciosjava;
+
+/**
+ *
+ * @author palsandi
+ */
+public class EjerciciosJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        // TODO code application logic here
+    
+    String nombre= "Jesús Navarro Sanchez";
+    int edad= 29;
+    double altura=1.65;
+    int peso= 91;
+    String complexion= "Fornido";
+    String localizacion= "La Peseta";
+    String profesion= "Chatarrero";
+    String Hobby= "Jardinería";
+    
+    System.out.println ("Me llamo " + nombre + ", tengo " + edad+ " años,mido " + altura + ", peso " + peso + ", Mi complexión es " + complexion + ", Vivo en " + localizacion + ", De profesión soy " + profesion + ", Uno de mis hobbies es la " + Hobby);
+            
+    
+    
+    
+        
+        
+        
+        
+    }
+    
+    
+}
